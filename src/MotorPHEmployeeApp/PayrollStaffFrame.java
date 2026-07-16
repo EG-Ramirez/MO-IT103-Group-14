a30e2c999be8baced92f42786258bc95f7b92bc5
Rl-Carreon <lr.rlcarreon@mmdc.mcl.edu.ph>
2026-07-17 00:17:14 +0800

    Remove dead code (exitButton.setBackground)

--- src/MotorPHEmployeeApp/PayrollStaffFrame.java
+++ src/MotorPHEmployeeApp/PayrollStaffFrame.java
@@ -98,8 +98,6 @@ public class PayrollStaffFrame extends JFrame {
         }
 
         // Exit button color
-        exitButton.setBackground(new Color(0, 128, 215));
-
         cardPanel.add(titleLabel, BorderLayout.NORTH);
         cardPanel.add(buttonPanel, BorderLayout.CENTER);
 
